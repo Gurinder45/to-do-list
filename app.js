7c186629b4ee726a91f80d95e6d8bc55235e0de2
Gurinder Bhogal <73514401+Gurinder45@users.noreply.github.com>
2022-07-28 17:50:40 -0700

    Add submit button functionality

---
+++ app.js
@@ -0,0 +1,28 @@
+// Selectors
+const taskInput = document.querySelector(".task-input");
+const submitBtn = document.querySelector(".submit-btn");
+const taskList = document.querySelector(".task-list");
+
+submitBtn.addEventListener("click", function (event) {
+  // Stop page from refreshing
+  event.preventDefault();
+  // create div to hold a task, done button, and a delete button
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("tasks");
+
+  const newTasks = document.createElement("li");
+  newTasks.classList.add("new-task");
+  taskDiv.appendChild(newTasks);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  doneBtn.classList.add("done-btn");
+  taskDiv.appendChild(doneBtn);
+
+  const deleteBtn = document.createElement("button");
+  doneBtn.innerHTML = '<i class="fa-solid fa-x"></i>';
+  doneBtn.classList.add("delete-btn");
+  taskDiv.appendChild(deleteBtn);
+
+  taskList.appendChild(taskDiv);
+});

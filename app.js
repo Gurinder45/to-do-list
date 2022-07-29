37f41a2bb0120edc8bc3aecc1e4c8ec5fbf07d02
Gurinder Bhogal <73514401+Gurinder45@users.noreply.github.com>
2022-07-29 13:07:01 -0700

    Add local storage functionality

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const taskInput = document.querySelector(".task-input");
 const submitBtn = document.querySelector(".submit-btn");
 const taskList = document.querySelector(".task-list");
 
-submitBtn.addEventListener("click", function (event) {
-  // Stop page from refreshing
-  event.preventDefault();
-  // create div to hold a task, done button, and a delete button
+document.addEventListener("DOMContentLoaded", getTasks);
+
+function createTask(text) {
+  // create div to hold a task and append it onto the tasklist
   const taskDiv = document.createElement("div");
   taskDiv.classList.add("task");
 
   const newTasks = document.createElement("li");
-  newTasks.innerText = taskInput.value;
+  newTasks.innerText = text;
   newTasks.classList.add("new-task");
   taskDiv.appendChild(newTasks);
 
@@ -26,7 +26,14 @@ submitBtn.addEventListener("click", function (event) {
   taskDiv.appendChild(deleteBtn);
 
   taskList.appendChild(taskDiv);
-  //clear value in input fie ld
+}
+
+submitBtn.addEventListener("click", function (event) {
+  // Stop page from refreshing
+  event.preventDefault();
+  createTask(taskInput.value);
+  localSave(taskInput.value);
+  //clear value in input field
   taskInput.value = "";
 });
 
@@ -35,6 +42,7 @@ taskList.addEventListener("click", function (event) {
   const selected = event.target;
   if (selected.classList[0] === "delete-btn") {
     const task = selected.parentElement;
+    localDelete(task);
     task.remove();
   }
   if (selected.classList[0] === "done-btn") {
@@ -42,3 +50,41 @@ taskList.addEventListener("click", function (event) {
     task.classList.toggle("finished-task");
   }
 });
+
+function localSave(task) {
+  // see if we already have tasks in local storage
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTasks() {
+  // see if we already have tasks in local storage
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task) {
+    createTask(task);
+  });
+}
+
+function localDelete(task) {
+  // see if we already have tasks in local storage
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  const index = task.children[0].innerText;
+  tasks.splice(tasks.indexOf(index), 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
